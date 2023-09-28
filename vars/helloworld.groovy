19c4cacf62241775d69c3dd4a639d58b949e5e7e
michalsalasa <michal.salasa@onwelo.com>
2023-09-28 10:25:26 +0200

    edit library

--- vars/helloworld.groovy
+++ vars/helloworld.groovy
@@ -1,4 +1,4 @@
 def call(Map config = [:]) {
 // def call(String typ , String level) {
-        sh ""Typ of pipeline: ${config.typ}. Level of advance: ${config.level}.""
+        sh "Typ of pipeline ${config.typ}. Level of advance ${config.level}."
 }
