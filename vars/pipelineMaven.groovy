25aea61c6f1ca90b1820a04152818be92b6eaac5
michalsalasa <michal.salasa@onwelo.com>
2023-10-05 09:46:41 +0200

    add mvn library

--- vars/pipelineMaven.groovy
+++ vars/pipelineMaven.groovy
@@ -1,14 +1,14 @@
 def call(Map settings = [:]) {
-    node {
-        ansiColor('xterm')
-        {
-        def skipTests = settings.get('skipTests', false)
-        def skipInstall = settings.get('skipInstall', false)
+    // node {
+    //     ansiColor('xterm')
+    //     {
+    //     def skipTests = settings.get('skipTests', false)
+    //     def skipInstall = settings.get('skipInstall', false)
 
-            // stage('Download Source Code') {
-            //         // checkout ([$class: "GitSCM", branches: [[name: 'main']], userRemoteConfigs:[[url: "https://github.com/michalsalasa/spring-petclinic.git"]]])
-            //         checkout scm
-            }
+    //         // stage('Download Source Code') {
+    //         //         // checkout ([$class: "GitSCM", branches: [[name: 'main']], userRemoteConfigs:[[url: "https://github.com/michalsalasa/spring-petclinic.git"]]])
+    //         //         checkout scm
+    //         }
             stage('Build') {
                 timestamps {
                     // Zbudowanie kodu
