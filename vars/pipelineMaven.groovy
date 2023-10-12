52e623f6874763d6662e36c0dc4b88139bfb3514
michalsalasa <michal.salasa@onwelo.com>
2023-10-12 10:35:48 +0200

    mvn issue

--- vars/pipelineMaven.groovy
+++ vars/pipelineMaven.groovy
@@ -4,34 +4,23 @@ def call(Map settings = [:]) {
         {
         def skipTests = settings.get('skipTests', false)
         def skipInstall = settings.get('skipInstall', false)
-    // environment {
-    //     M2_HOME = '/usr/share/java/maven-3'
-    //     PATH = "${env.M2_HOME}/bin:${env.PATH}"
-    //     // skipTests = params.skipTests ?: false
-    //     // skipInstall = params.skipInstall ?: false
-    // }
-        // stage('Download Source Code') {
-        //         checkout scm
-        // }
        
         stage('BuildMVN') {
             timestamps {
-                // Zbudowanie kodu... timestamps instead of withMaven
-                
+                // Zbudowanie kodu...
                 sh '''
                 cd /home/jenkins/jenkins_slave/workspace/JenPipeline
                 pwd
                 ls                
                 export MAVEN_HOME='/opt/apache-maven-3.6.3/bin'
                 mvn package -DskipTests
-
                 '''
             }
-        }                // mvn package -DskipTests.......export MAVEN_HOME='/opt/apache-maven-3.6.3/bin'
+        }
         stage('TestMVN') {
             if (!skipTests)
             script {
-                // Uruchomienie testów aplikacji
+                // testy
                 sh  '''
                 cd /home/jenkins/jenkins_slave/workspace/JenPipeline
                 mvn verify '''
@@ -47,8 +36,7 @@ def call(Map settings = [:]) {
                 // Install artefakt w lokalnym repo .m2
                 sh '''
                 cd /home/jenkins/jenkins_slave/workspace/JenPipeline
-                mvn install -DskipTests '''
-            
+                mvn install -DskipTests '''           
         }
     }
 }
